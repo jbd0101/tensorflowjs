df38157cbd8b5220ac97b3a6866a3aa68a0a2d5a
jean-christophe bauduin <bauduinjc@gmail.com>
2019-04-03 18:21:44 +0200

    Updates

---
+++ precache-manifest.bbb5cd45461ed880f6986576afc97cb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tensorflowjs/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "992f05a2cfdad3d2a8cc",
+    "url": "/tensorflowjs/static/js/runtime~main.2c1ced04.js"
+  },
+  {
+    "revision": "8020d09cbd0eabc1736f",
+    "url": "/tensorflowjs/static/js/main.971f53a3.chunk.js"
+  },
+  {
+    "revision": "f097eda8b4911ce4d189",
+    "url": "/tensorflowjs/static/js/2.ea674b91.chunk.js"
+  },
+  {
+    "revision": "8020d09cbd0eabc1736f",
+    "url": "/tensorflowjs/static/css/main.05aa7866.chunk.css"
+  },
+  {
+    "revision": "0ec1a9bd67420d2697934e86f3684616",
+    "url": "/tensorflowjs/index.html"
+  }
+];
